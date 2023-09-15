f9f6bdec243b376cb92b90d25543d79c6e6f7a8b
amlanjyoti.sahoo <amlanjyoti.sahoo@godigit.com>
2023-09-16 01:29:22 +0530

    code clean

--- src/screens/Pomodoro.js
+++ src/screens/Pomodoro.js
@@ -2,65 +2,63 @@ import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, Dimensions, Image} from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
 
+const data = [
+  {
+    title: 'Aenean leo',
+    body: 'Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+    imgUrl: 'https://picsum.photos/id/11/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'In turpis',
+    body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
+    imgUrl: 'https://picsum.photos/id/10/200/300',
+  },
+  {
+    title: 'Lorem Ipsum',
+    body: 'Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.',
+    imgUrl: 'https://picsum.photos/id/12/200/300',
+  },
+];
+
 const Pomodoro = () => {
-  const isCarousel = React.useRef(null);
-
-  const data = [
-    {
-      title: 'Aenean leo',
-      body: 'Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
-      imgUrl: 'https://picsum.photos/id/11/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'In turpis',
-      body: 'Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ',
-      imgUrl: 'https://picsum.photos/id/10/200/300',
-    },
-    {
-      title: 'Lorem Ipsum',
-      body: 'Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.',
-      imgUrl: 'https://picsum.photos/id/12/200/300',
-    },
-  ];
-
   const [index, setIndex] = React.useState(0);
-  const [nextIndex, setnextIndex] = React.useState(index + 1);
+  const isCarousel = React.useRef(null);
 
   const SLIDER_WIDTH = Dimensions.get('window').width + 80;
   const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
@@ -98,7 +96,6 @@ const Pomodoro = () => {
         itemWidth={ITEM_WIDTH}
         onSnapToItem={index => {
           setIndex(index);
-          setnextIndex(index + 1);
         }}
         useScrollView={true}
       />
